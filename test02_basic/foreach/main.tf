d81f0533e5f209812a2ac54a36d6c67f78ad4e4d
Mind Mug <mindmug2018@gmail.com>
2026-04-29 12:36:10 +0900

    for each added

---
+++ test02_basic/foreach/main.tf
@@ -0,0 +1,18 @@
+# terraform_example/test02_basic/foreach/main.tf
+# local_file.students_notes is map type
+# for_each can be set type or map type
+
+locals {
+  students = ["chihiro", "junhan", "shinichiro"]
+}
+
+resource "local_file" "student_notes" {
+    for_each = toset(local.students)
+    filename = "${path.module}/student_${each.key}.txt"
+    content = "Hello! This is the noe of ${each.value}"
+}
+
+output "debug" {
+    description = "all contents that were created"
+    value = [for item in local_file.student_notes : item.filename]
+}
