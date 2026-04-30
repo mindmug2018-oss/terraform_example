4a21433f0d66b247d6e904bc9038825c239875d2
Mind Mug <mindmug2018@gmail.com>
2026-04-30 09:33:46 +0900

    locals added

---
+++ test02_basic/locals/variables.tf
@@ -0,0 +1,12 @@
+# terraform_example/test02_basic/locals/variables.tf
+
+variable "env" {
+    type = string
+    description = "current environment (dev | prod)"
+}
+
+variable "project_name" {
+    type = string
+    description = "project name"
+}
+
